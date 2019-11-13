4c577e1ca999d30cd83ee5fd23f402ca99f3cdb3
Fabian Bösch <48126478+boeschf@users.noreply.github.com>
2019-11-13 13:46:15 +0100

    better test

--- tests/transport/test_ts.cpp
+++ tests/transport/test_ts.cpp
@@ -23,8 +23,10 @@ using callback_comm_t = gridtools::ghex::tl::callback_communicator_ts<comm_t,std
 
 std::atomic<std::size_t> num_completed;
 
-void test1(std::size_t num_progress_threads)
+void test1(std::size_t num_progress_threads, bool wait)
 {
+    num_completed.store(0u);
+
     comm_t comm;
     callback_comm_t cb_comm(comm); 
 
@@ -39,14 +41,24 @@ void test1(std::size_t num_progress_threads)
 
     std::size_t num_requests = 2;
 
-    auto send_recv_func =
+    // lambda which places send and receive calls
+    auto send_recv_func_nowait =
+    [&cb_comm,&recv_msg,&send_msg,l_rank,r_rank]()
+    {
+        cb_comm.recv(recv_msg,l_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "received!" << std::endl; });
+        cb_comm.send(send_msg,r_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "sent!" << std::endl; });
+    };
+    
+    // lambda which places send and receive calls and waits for completion
+    auto send_recv_func_wait =
     [&cb_comm,&recv_msg,&send_msg,&recv_req,&send_req,l_rank,r_rank]()
     {
-        /*recv_req =*/ cb_comm.recv(recv_msg,l_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "received!" << std::endl; });
-        /*send_req =*/ cb_comm.send(send_msg,r_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "sent!" << std::endl; });
-        //while ( !(recv_req.is_ready() && send_req.is_ready()) ) {}
+        recv_req = cb_comm.recv(recv_msg,l_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "received!" << std::endl; });
+        send_req = cb_comm.send(send_msg,r_rank,0,[](callback_comm_t::message_type, int, int) { std::cout << "sent!" << std::endl; });
+        while ( !(recv_req.is_ready() && send_req.is_ready()) ) {}
     };
 
+    // lambda which progresses the queues
     auto progress_func =
     [&cb_comm, num_requests]()
     {
@@ -56,20 +68,25 @@ void test1(std::size_t num_progress_threads)
         }
     };
 
-    std::vector<std::thread> progress_threads;
-    progress_threads.reserve(num_progress_threads+1);
+    std::vector<std::thread> threads;
+    threads.reserve(num_progress_threads+1);
 
     for (std::size_t i=0; i<num_progress_threads; ++i)
-        progress_threads.push_back( std::thread(progress_func) );
-        
-    progress_threads.push_back( std::thread(send_recv_func) );
+        threads.push_back( std::thread(progress_func) );
 
+    if (wait) 
+        threads.push_back( std::thread(send_recv_func_wait) );
+    else
+        threads.push_back( std::thread(send_recv_func_nowait) );
 
-    for (auto& t : progress_threads)
+    for (auto& t : threads)
         t.join();
+
+    comm.barrier();
 }
 
 
 TEST(transport, basic) {
-    test1(2);
+    test1(2, true);
+    test1(2, false);
 }
